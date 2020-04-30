3ec28604d4f352a31e9dbff72baeee6d57a2f717
hatch <hatch.tech.eng@gmail.com>
2020-04-30 13:08:19 +0900

    Inspectorを表示するのを追加

---
+++ Script/Editor/InspectorDrawer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Yorozu.CustomProperty
+{
+	[CustomPropertyDrawer(typeof(InspectorAttribute))]
+	[CustomPropertyDrawer(typeof(Text))]
+	[CustomPropertyDrawer(typeof(Image))]
+	public class InspectorDrawer : PropertyDrawer
+	{
+		private Editor _cacheEditor;
+		
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			position.height = base.GetPropertyHeight(property, label);
+			EditorGUI.PropertyField(position, property, label);
+			if (property.objectReferenceValue == null)
+				return;
+
+			property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+
+			if (!property.isExpanded)
+				return;
+			
+			if (_cacheEditor == null)
+				_cacheEditor = Editor.CreateEditor(property.objectReferenceValue);
+
+			using (new EditorGUI.IndentLevelScope())
+			{
+				using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox))
+				{
+					_cacheEditor.OnInspectorGUI();
+				}
+			}
+		}
+	}
+}
